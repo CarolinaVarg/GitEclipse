8057608862de7e592f69d4256f80930e42aed8f8
carolinavarg <67013069+carolinavarg@users.noreply.github.com>
2023-07-22 18:15:28 -0500

    Update Ejemplo.java

--- src/test/java/Ejemplo.java
+++ src/test/java/Ejemplo.java
@@ -4,9 +4,12 @@ public class Ejemplo {
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo");
 
+		String  apellido = "Apellido";
 		String nombre = "Carolina";
 
 		System.out.println(nombre);
+
+		
 	}
 
 }
