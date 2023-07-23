b634aaacf9a63554b044876857e5fe324669e42b
Carolina Vargas Alcala <carolina.varg15@hotmail.com>
2023-07-23 00:21:24 -0500

    agregar apellido

--- src/test/java/Ejemplo.java
+++ src/test/java/Ejemplo.java
@@ -4,7 +4,9 @@ public class Ejemplo {
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo");
 		String nombre = "Carolina";
+		String apellido ="Vargas";
 		System.out.println(nombre);
+		System.out.println(apellido);
 
 		
 	}
